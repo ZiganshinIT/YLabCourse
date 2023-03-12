e770ccafef718c0eefdf62f7af1f4601ef16581f
ZiganshinIT <it.ziganshin@gmail.com>
2023-03-12 19:25:23 +0300

    Added files of the 5th task

---
+++ src/week2/statsAccumulator/StatsAccumulatorImpl.java
@@ -0,0 +1,44 @@
+package week2.statsAccumulator;
+
+public class StatsAccumulatorImpl implements StatsAccumulator{
+
+    private int minValue;
+    private int maxValue;
+    private int count = 0;
+    private double sum = 0;
+    private Double avg = 0.0;
+
+    @Override
+    public void add(int value) {
+        if (count == 0) {
+            minValue = value;
+            maxValue = value;
+        } else {
+            minValue = Math.min(value, minValue);
+            maxValue = Math.max(value, maxValue);
+        }
+        sum += value;
+        count++;
+        avg = sum / count;
+    }
+
+    @Override
+    public int getMin() {
+        return minValue;
+    }
+
+    @Override
+    public int getMax() {
+        return maxValue;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public Double getAvg() {
+        return avg;
+    }
+}
---
+++ src/week2/statsAccumulator/StatsAccumulatorTest.java
@@ -0,0 +1,16 @@
+package week2.statsAccumulator;
+
+public class StatsAccumulatorTest {
+    public static void main(String[] args) {
+        StatsAccumulatorImpl s = new StatsAccumulatorImpl();
+        s.add(1);
+        s.add(2);
+        System.out.println(s.getAvg()); // 1.5 - среднее арифметическоеэлементов
+        s.add(0);
+        System.out.println(s.getMin()); // 0 - минимальное из переданныхзначений
+        s.add(3);
+        s.add(8);
+        System.out.println(s.getMax()); // 8 - максимальный из переданных
+        System.out.println(s.getCount()); // 5 - количество переданных элементов
+    }
+}
