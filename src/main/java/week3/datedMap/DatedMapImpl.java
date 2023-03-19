68dc2020ad65902644f52199391c26a7a7db5169
ZiganshinIT <it.ziganshin@gmail.com>
2023-03-19 12:43:40 +0300

    Added file of the 2nd task (3 week) (DatedMap)

---
+++ src/main/java/week3/datedMap/DatedMapImpl.java
@@ -0,0 +1,48 @@
+package main.java.week3.datedMap;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+
+public class DatedMapImpl implements DatedMap {
+    private final Map<String, String> map = new HashMap<>();
+    private final Map<String, Date> keyLastInsertionDate = new HashMap<>();
+
+    @Override
+    public void put(String key, String value) {
+        map.put(key, value);
+        keyLastInsertionDate.put(key, new Date());
+    }
+
+    @Override
+    public String get(String key) {
+        return map.get(key);
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public void remove(String key) {
+        map.remove(key);
+        keyLastInsertionDate.remove(key);
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return map.keySet();
+    }
+
+    @Override
+    public Date getKeyLastInsertionDate(String key) {
+        return keyLastInsertionDate.get(key);
+    }
+
+    @Override
+    public String toString() {
+        return map.toString();
+    }
+}
---
+++ src/main/java/week3/datedMap/DatedMapTest.java
@@ -0,0 +1,27 @@
+package main.java.week3.datedMap;
+
+public class DatedMapTest {
+    public static void main(String[] args) throws InterruptedException {
+        DatedMapImpl datedMap = new DatedMapImpl();
+        datedMap.put("A", "a");
+        datedMap.put("B", "b");
+        datedMap.put("C", "c");
+
+        System.out.println(datedMap.get("B"));
+        System.out.println(datedMap.get("F"));
+
+        System.out.println(datedMap.containsKey("C"));
+        System.out.println(datedMap.containsKey("Y"));
+
+        System.out.println(datedMap.keySet());
+
+        System.out.println(datedMap.getKeyLastInsertionDate("A"));
+
+        datedMap.remove("A");
+        datedMap.remove("D");
+
+        System.out.println(datedMap.getKeyLastInsertionDate("A"));
+
+
+    }
+}
